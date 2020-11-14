133cd9861444e8bbab92c608c527dc6cab833dc1
Jörg Singer <singer.joerg@gmx.de>
2020-11-14 11:00:26 +0100

    Test added for node ordering problem.

--- tests/embedder_tests.rs
+++ tests/embedder_tests.rs
@@ -104,3 +104,77 @@ fn more_complex_tree() {
         assert_eq!(2, e.x_extend_children);
     }
 }
+
+#[test]
+fn moved_nodes() {
+    //      0 ---------
+    //     / \    \    \
+    //    1   2    4    3
+    let mut tree: Tree<MyNodeData> = TreeBuilder::new().with_node_capacity(5).build();
+
+    let root_id: NodeId = tree.insert(Node::new(MyNodeData(0)), AsRoot).unwrap();
+    let n1_id: NodeId = tree
+        .insert(Node::new(MyNodeData(1)), UnderNode(&root_id))
+        .unwrap();
+    tree.insert(Node::new(MyNodeData(2)), UnderNode(&root_id))
+        .unwrap();
+    let n4_id = tree
+        .insert(Node::new(MyNodeData(4)), UnderNode(&root_id))
+        .unwrap();
+    let n3_id = tree.insert(Node::new(MyNodeData(3)), UnderNode(&root_id))
+        .unwrap();
+
+    //      0
+    //     / \
+    //    1   2
+    //   / \
+    //  3   4
+    let _ = tree.move_node(&n3_id, MoveBehavior::ToParent(&n1_id));
+    let _ = tree.move_node(&n4_id, MoveBehavior::ToParent(&n1_id));
+
+    let embedding = Embedder::embed(&tree);
+
+    assert!(!embedding.is_empty());
+    assert_eq!(5, embedding.len());
+
+    {
+        let e = &embedding[0];
+        assert_eq!("0".to_string(), e.name);
+        assert_eq!(0, e.y_order);
+        assert_eq!(3, e.x_center);
+        assert_eq!(2, e.x_extend);
+        assert_eq!(6, e.x_extend_children);
+    }
+    {
+        let e = &embedding[1];
+        assert_eq!("1".to_string(), e.name);
+        assert_eq!(1, e.y_order);
+        assert_eq!(2, e.x_center);
+        assert_eq!(2, e.x_extend);
+        assert_eq!(4, e.x_extend_children);
+    }
+    {
+        let e = &embedding[2];
+        assert_eq!("2".to_string(), e.name);
+        assert_eq!(1, e.y_order);
+        assert_eq!(5, e.x_center);
+        assert_eq!(2, e.x_extend);
+        assert_eq!(2, e.x_extend_children);
+    }
+    {
+        let e = &embedding[3];
+        assert_eq!("4".to_string(), e.name);
+        assert_eq!(2, e.y_order);
+        assert_eq!(3, e.x_center);
+        assert_eq!(2, e.x_extend);
+        assert_eq!(2, e.x_extend_children);
+    }
+    {
+        let e = &embedding[4];
+        assert_eq!("3".to_string(), e.name);
+        assert_eq!(2, e.y_order);
+        assert_eq!(1, e.x_center);
+        assert_eq!(2, e.x_extend);
+        assert_eq!(2, e.x_extend_children);
+    }
+}
